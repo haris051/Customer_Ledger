f364b6c18d1dc331d7019a954956c8dbaedc2fdb
Muhammad Harris <96321391+haris051@users.noreply.github.com>
2024-09-03 18:22:00 +0500

    Update PROC_CUSTOMER_LEDGER.sql
    Receipts Section is Changed in beginning balance and non beginning balance

--- PROC_CUSTOMER_LEDGER.sql
+++ PROC_CUSTOMER_LEDGER.sql
@@ -17,242 +17,197 @@ BEGIN
     
     -- =========== Beginning Balance ===========
 
-	SELECT CASE 
-				WHEN ENTRY_DATE IS NOT NULL THEN IFNULL(SUM(BALANCE), 0)
-				ELSE IFNULL(SUM(DEBIT), 0) - IFNULL(SUM(CREDIT), 0)
-		   END AS BALANCE INTO BEGININGBALANCE
-	  FROM (SELECT CUSTOMER_ID, 
-				   ENTRY_DATE, 
-                   PAYPAL_TRANSACTION_ID, 
-                   FLAG, 
-                   FORM, 
-                   DEBIT, 
-                   CREDIT, 
-                   FINAL,
-				   SUM(FINAL) OVER (PARTITION BY CUSTOMER_ID ORDER BY CUSTOMER_ID, ENTRY_DATE, PAYPAL_TRANSACTION_ID, FLAG, FORM, DEBIT, CREDIT, FINAL) AS BALANCE
-			  FROM ( SELECT B.CUSTOMER_ID, 
-							A.SI_ENTRY_DATE AS ENTRY_DATE, 
-							A.PAYPAL_TRANSACTION_ID, 
-							'V' AS FLAG, 
-							'Sale Invoice' AS FORM,
+			  SELECT  
+              
+					IFNULL(SUM(DEBIT),0) - IFNULL(SUM(CREDIT),0) INTO BEGININGBALANCE
+                   
+				   
+			  FROM ( 
+					 SELECT A.CUSTOMER_ID, 
 							SUM(A.SI_TOTAL) AS DEBIT,
 							NULL AS CREDIT,
-							SUM(A.SI_TOTAL) AS FINAL
-					   FROM SALE_INVOICE A,
-							CUSTOMER B
-					  WHERE A.CUSTOMER_ID = B.ID
-						AND CASE
+							SUM(A.SI_TOTAL) AS FINAL,
+							'V' AS FLAG
+					   FROM SALE_INVOICE A
+					   where
+						   CASE
 							   WHEN P_ENTRY_DATE_FROM <> "" THEN A.SI_ENTRY_DATE < P_ENTRY_DATE_FROM
 							   ELSE TRUE
 							END
 						AND CASE
-							   WHEN P_CUSTOMER_ID <> "" THEN B.ID = P_CUSTOMER_ID
+							   WHEN P_CUSTOMER_ID <> "" THEN A.CUSTOMER_ID = P_CUSTOMER_ID
 							   ELSE TRUE
 							END
 						AND CASE
 							   WHEN P_COMPANY_ID <> "" THEN A.COMPANY_ID = P_COMPANY_ID
 							   ELSE TRUE
 							END
-				   GROUP BY B.CUSTOMER_ID, A.SI_ENTRY_DATE, A.PAYPAL_TRANSACTION_ID
+				   GROUP BY A.CUSTOMER_ID
 						 
 					 UNION ALL 
 				   
-					 SELECT B.CUSTOMER_ID, 
-							A.ST_ENTRY_DATE AS ENTRY_DATE, 
-							A.ST_ID AS PAYPAL_TRANSACTION_ID, 
-							'C' AS FLAG, 
-							'Stock Out' AS FORM,
+					 SELECT A.CUSTOMER_ID,  
 							SUM(A.ST_TOTAL) AS DEBIT,
 							NULL AS CREDIT,
-							SUM(A.ST_TOTAL) AS FINAL
-					   FROM VW_STOCK_TRANSFER A,
-							CUSTOMER B
-					  WHERE A.CUSTOMER_ID = B.ID
-						AND CASE
+							SUM(A.ST_TOTAL) AS FINAL,
+							'C' AS FLAG
+					   FROM VW_STOCK_TRANSFER A
+					  WHERE 
+						    CASE
 							   WHEN P_ENTRY_DATE_FROM <> "" THEN A.ST_ENTRY_DATE < P_ENTRY_DATE_FROM
 							   ELSE TRUE
 							END
 						AND CASE
-							   WHEN P_CUSTOMER_ID <> "" THEN B.ID = P_CUSTOMER_ID
+							   WHEN P_CUSTOMER_ID <> "" THEN A.CUSTOMER_ID = P_CUSTOMER_ID
 							   ELSE TRUE
 							END
 						AND CASE
 							   WHEN P_COMPANY_ID <> "" THEN A.COMPANY_FROM_ID = P_COMPANY_ID
 							   ELSE TRUE
 							END
-				   GROUP BY B.CUSTOMER_ID, A.ST_ENTRY_DATE, A.ST_ID
+				   GROUP BY A.CUSTOMER_ID
 						  
 					 UNION ALL
 					
-					 SELECT B.CUSTOMER_ID, 
-							A.PS_ENTRY_DATE AS ENTRY_DATE, 
-							A.PAYPAL_TRANSACTION_ID, 
-							'P' AS FLAG,
-							'Payment Sent' AS FORM,
+					 SELECT A.CUSTOMER_ID, 
 							SUM(A.AMOUNT) AS DEBIT,
 							NULL AS CREDIT,
-							SUM(A.AMOUNT) AS FINAL
-					   FROM PAYMENT_SENT A,
-							CUSTOMER B
-					  WHERE A.CUSTOMER_ID = B.ID
-						AND CASE
+							SUM(A.AMOUNT) AS FINAL,
+							'P' AS FLAG
+					   FROM PAYMENT_SENT A
+					   where
+						    CASE
 							   WHEN P_ENTRY_DATE_FROM <> "" THEN A.PS_ENTRY_DATE < P_ENTRY_DATE_FROM
 							   ELSE TRUE
 							END
 						AND CASE
-							   WHEN P_CUSTOMER_ID <> "" THEN B.ID = P_CUSTOMER_ID
+							   WHEN P_CUSTOMER_ID <> "" THEN A.CUSTOMER_ID = P_CUSTOMER_ID
 							   ELSE TRUE
 							END
 						AND CASE
 							   WHEN P_COMPANY_ID <> "" THEN A.COMPANY_ID = P_COMPANY_ID
 							   ELSE TRUE
 							END
-				   GROUP BY B.CUSTOMER_ID, A.PS_ENTRY_DATE, A.PAYPAL_TRANSACTION_ID
+				    GROUP BY A.CUSTOMER_ID
 						  
 					 UNION ALL 
 					
-					 SELECT B.CUSTOMER_ID, 
-							A.PC_ENTRY_DATE AS ENTRY_DATE, 
-							A.PAYPAL_TRANSACTION_ID, 
-							'R' AS FLAG, 
-							'Partial Credit' AS FORM,
+					 SELECT A.CUSTOMER_ID, 
 							NULL AS DEBIT,
 							SUM(A.TOTAL_AMOUNT) AS CREDIT,
-							SUM(A.TOTAL_AMOUNT * -1) AS FINAL
-					   FROM PARTIAL_CREDIT A,
-							CUSTOMER B
-					  WHERE A.CUSTOMER_ID = B.ID
-						AND  CASE
+							SUM(A.TOTAL_AMOUNT * -1) AS FINAL,
+							'R' AS FLAG
+					   FROM PARTIAL_CREDIT A
+					   where
+						    CASE
 							   WHEN P_ENTRY_DATE_FROM <> "" THEN A.PC_ENTRY_DATE < P_ENTRY_DATE_FROM
 							   ELSE TRUE
 							END
 						AND CASE
-							   WHEN P_CUSTOMER_ID <> "" THEN B.ID = P_CUSTOMER_ID
+							   WHEN P_CUSTOMER_ID <> "" THEN A.CUSTOMER_ID = P_CUSTOMER_ID
 							   ELSE TRUE
 							END
 						AND CASE
 							   WHEN P_COMPANY_ID <> "" THEN A.COMPANY_ID = P_COMPANY_ID
 							   ELSE TRUE
 							END
-				   GROUP BY B.CUSTOMER_ID, A.PC_ENTRY_DATE, A.PAYPAL_TRANSACTION_ID
+				   GROUP BY A.CUSTOMER_ID
 						  
 					 UNION ALL 
 				   
-					 SELECT B.CUSTOMER_ID, 
-							A.SR_ENTRY_DATE AS ENTRY_DATE, 
-							A.PAYPAL_TRANSACTION_ID, 
-							'Q' AS FLAG, 
-							'Sale Return' AS FORM,
+					 SELECT A.CUSTOMER_ID,
 							NULL AS DEBIT,
 							SUM(A.SR_TOTAL) AS CREDIT,
-							SUM(A.SR_TOTAL * -1) AS FINAL
-					   FROM SALE_RETURN A,
-							CUSTOMER B
-					  WHERE A.CUSTOMER_ID = B.ID
-						AND  CASE
+							SUM(A.SR_TOTAL * -1) AS FINAL,
+							'Q' AS FLAG
+					   FROM SALE_RETURN A
+					  WHERE 
+							CASE
 							   WHEN P_ENTRY_DATE_FROM <> "" THEN A.SR_ENTRY_DATE < P_ENTRY_DATE_FROM
 							   ELSE TRUE
 							END
 						AND CASE
-							   WHEN P_CUSTOMER_ID <> "" THEN B.ID = P_CUSTOMER_ID
+							   WHEN P_CUSTOMER_ID <> "" THEN A.CUSTOMER_ID = P_CUSTOMER_ID
 							   ELSE TRUE
 							END
 						AND CASE
 							   WHEN P_COMPANY_ID <> "" THEN A.COMPANY_ID = P_COMPANY_ID
 							   ELSE TRUE
 							END
-				   GROUP BY B.CUSTOMER_ID, A.SR_ENTRY_DATE, A.PAYPAL_TRANSACTION_ID
+				   GROUP BY A.CUSTOMER_ID
 						  
 					 UNION ALL
 
-					 SELECT B.CUSTOMER_ID, 
-							A.RM_ENTRY_DATE AS ENTRY_DATE, 
-							A.PAYPAL_TRANSACTION_ID, 
-							'M' AS FLAG, 
-							'Receive Money' AS FORM,
+					 SELECT A.CUSTOMER_ID, 
 							NULL AS DEBIT,
 							SUM(A.AMOUNT) AS CREDIT,
-							SUM(A.AMOUNT * -1) AS FINAL
-					   FROM RECEIVE_MONEY A,
-							CUSTOMER B
-					  WHERE A.CUSTOMER_ID = B.ID
-						AND  CASE
+							SUM(A.AMOUNT * -1) AS FINAL,
+							'M' AS FLAG
+					   FROM RECEIVE_MONEY A
+					  WHERE 
+							CASE
 							   WHEN P_ENTRY_DATE_FROM <> "" THEN A.RM_ENTRY_DATE < P_ENTRY_DATE_FROM
 							   ELSE TRUE
 							END
 						AND CASE
-							   WHEN P_CUSTOMER_ID <> "" THEN B.ID = P_CUSTOMER_ID
+							   WHEN P_CUSTOMER_ID <> "" THEN A.CUSTOMER_ID = P_CUSTOMER_ID
 							   ELSE TRUE
 							END
 						AND CASE
 							   WHEN P_COMPANY_ID <> "" THEN A.COMPANY_ID = P_COMPANY_ID
 							   ELSE TRUE
 							END
-				   GROUP BY B.CUSTOMER_ID, A.RM_ENTRY_DATE, A.PAYPAL_TRANSACTION_ID
+				   GROUP BY A.CUSTOMER_ID
 						  
 					 UNION ALL
 
-					 SELECT B.CUSTOMER_ID, 
-							A.REP_ENTRY_DATE AS ENTRY_DATE, 
-							A.PAYPAL_TRANSACTION_ID, 
-							'X' AS FLAG, 
-							'Replacement Issue' AS FORM,
+					 SELECT A.CUSTOMER_ID, 
 							SUM(A.BALANCE) AS DEBIT,
 							NULL AS CREDIT,
-							SUM(A.BALANCE) AS FINAL
-					   FROM REPLACEMENT A,
-							CUSTOMER B
+							SUM(A.BALANCE) AS FINAL,
+							'X' AS FLAG
+					   FROM REPLACEMENT A
 					  WHERE A.BALANCE >= 0
-						AND A.CUSTOMER_ID = B.ID
 						AND CASE
 							   WHEN P_ENTRY_DATE_FROM <> "" THEN A.REP_ENTRY_DATE < P_ENTRY_DATE_FROM
 							   ELSE TRUE
 							END
 						AND CASE
-							   WHEN P_CUSTOMER_ID <> "" THEN B.ID = P_CUSTOMER_ID
+							   WHEN P_CUSTOMER_ID <> "" THEN A.CUSTOMER_ID = P_CUSTOMER_ID
 							   ELSE TRUE
 							END
 						AND CASE
 							   WHEN P_COMPANY_ID <> "" THEN A.COMPANY_ID = P_COMPANY_ID
 							   ELSE TRUE
 							END
-				   GROUP BY B.CUSTOMER_ID, A.REP_ENTRY_DATE, A.PAYPAL_TRANSACTION_ID
+				   GROUP BY A.CUSTOMER_ID
 						  
 					 UNION ALL
 
-					 SELECT B.CUSTOMER_ID, 
-							A.REP_ENTRY_DATE AS ENTRY_DATE, 
-							A.PAYPAL_TRANSACTION_ID, 
-							'Y' AS FLAG,
-							'Replacement Return' AS FORM,
+					 SELECT A.CUSTOMER_ID, 
 							NULL AS DEBIT,
 							SUM(A.BALANCE * -1) AS CREDIT,
-							SUM(A.BALANCE) AS FINAL
-					   FROM REPLACEMENT A,
-							CUSTOMER B
+							SUM(A.BALANCE) AS FINAL,
+							'Y' AS FLAG
+					   FROM REPLACEMENT A
 					  WHERE A.BALANCE < 0
-						AND A.CUSTOMER_ID = B.ID
 						AND CASE
 							   WHEN P_ENTRY_DATE_FROM <> "" THEN A.REP_ENTRY_DATE < P_ENTRY_DATE_FROM
 							   ELSE TRUE
 							END
 						AND CASE
-							   WHEN P_CUSTOMER_ID <> "" THEN B.ID = P_CUSTOMER_ID
+							   WHEN P_CUSTOMER_ID <> "" THEN A.CUSTOMER_ID = P_CUSTOMER_ID
 							   ELSE TRUE
 							END
 						AND CASE
 							   WHEN P_COMPANY_ID <> "" THEN A.COMPANY_ID = P_COMPANY_ID
 							   ELSE TRUE
 							END
-				   GROUP BY B.CUSTOMER_ID, A.REP_ENTRY_DATE, A.PAYPAL_TRANSACTION_ID
+				   GROUP BY A.CUSTOMER_ID
 				   
 					 UNION ALL
 
-					 SELECT B.CUSTOMER_ID, 
-							A.R_ENTRY_DATE AS ENTRY_DATE, 
-							A.REC_REFERENCE, 
-							'T' AS FLAG,
-							'Customer Receipt' AS FORM,
+					 SELECT A.CUSTOMER_ID, 
 							CASE 
 								WHEN A.REMAINING_AMOUNT < 0 THEN SUM(ABS(A.REMAINING_AMOUNT)) 
 								ELSE NULL 
@@ -262,33 +217,31 @@ BEGIN
 								ELSE NULL  
 							END AS CREDIT,
 							CASE 
-								WHEN A.REMAINING_AMOUNT > 0 THEN SUM(ABS(A.REMAINING_AMOUNT)) 
-								ELSE SUM(ABS(A.REMAINING_AMOUNT) * -1)
-							END AS FINAL
-					   FROM RECEIPTS A,
-							CUSTOMER B
+								WHEN A.REMAINING_AMOUNT > 0 THEN SUM(ABS(A.REMAINING_AMOUNT)) *-1 
+								ELSE SUM(ABS(A.REMAINING_AMOUNT))
+							END AS FINAL,
+							'T' AS FLAG
+					   FROM RECEIPTS A
 					  WHERE A.REMAINING_AMOUNT <> 0
-						AND A.CUSTOMER_ID = B.ID
-						AND  CASE
+						AND
+							CASE
 							   WHEN P_ENTRY_DATE_FROM <> "" THEN A.R_ENTRY_DATE < P_ENTRY_DATE_FROM
 							   ELSE TRUE
 							END
 						AND CASE
-							   WHEN P_CUSTOMER_ID <> "" THEN B.ID = P_CUSTOMER_ID
+							   WHEN P_CUSTOMER_ID <> "" THEN A.CUSTOMER_ID = P_CUSTOMER_ID
 							   ELSE TRUE
 							END
 						AND CASE
 							   WHEN P_COMPANY_ID <> "" THEN A.COMPANY_ID = P_COMPANY_ID
 							   ELSE TRUE
 							END
-				   GROUP BY B.CUSTOMER_ID, A.R_ENTRY_DATE, A.REC_REFERENCE
+				   GROUP BY A.CUSTOMER_ID,A.REMAINING_AMOUNT
 				   ) b
 			 WHERE CASE
 						WHEN P_FORM_TYPE <> "-1" THEN FLAG IN (P_FORM_TYPE)
 						ELSE TRUE
-				   END
-		   ) C
-  GROUP BY CUSTOMER_ID;
+				   END group by B.CUSTOMER_ID;
   
 	-- =========== Beginning Balance ===========
 
@@ -323,13 +276,13 @@ BEGIN
 											    ''Beginning Balance'' AS FORM,
 											    NULL  AS DEBIT,
 											    NULL AS CREDIT,
-											    SUM(TOTAL_AMOUNT) + IFNULL((\'',BEGININGBALANCE,'\'),0) AS FINAL
+											    TOTAL_AMOUNT + IFNULL((\'',BEGININGBALANCE,'\'),0) AS FINAL
 										   FROM CUSTOMER
 										  WHERE CASE
 												   WHEN \'',P_CUSTOMER_ID,'\' <> "" THEN ID = \'',P_CUSTOMER_ID,'\'
 												   ELSE TRUE
 											    END
-									   GROUP BY CUSTOMER_ID, ENTRY_DATE,ID
+									   
                                                 
                                          UNION ALL
                                          
@@ -339,9 +292,9 @@ BEGIN
 											    A.PAYPAL_TRANSACTION_ID, 
 											    ''V'' AS FLAG, 
 											    ''Sale Invoice'' AS FORM,
-											    SUM(A.SI_TOTAL) AS DEBIT,
+											    A.SI_TOTAL AS DEBIT,
 											    NULL AS CREDIT,
-											    SUM(A.SI_TOTAL) AS FINAL
+											    A.SI_TOTAL AS FINAL
 										   FROM SALE_INVOICE A,
 											    CUSTOMER B
 										  WHERE A.CUSTOMER_ID = B.ID
@@ -361,7 +314,7 @@ BEGIN
 												   WHEN \'',P_COMPANY_ID,'\' <> "" THEN A.COMPANY_ID = \'',P_COMPANY_ID,'\'
 												   ELSE TRUE
 											    END
-									   GROUP BY B.CUSTOMER_ID, A.SI_ENTRY_DATE, A.PAYPAL_TRANSACTION_ID,A.ID
+									   
 											 
 									     UNION ALL 
 									   
@@ -371,9 +324,9 @@ BEGIN
 											    A.ST_ID AS PAYPAL_TRANSACTION_ID, 
 											    ''C'' AS FLAG, 
 											    ''Stock Out'' AS FORM,
-											    SUM(A.ST_TOTAL) AS DEBIT,
+											    A.ST_TOTAL AS DEBIT,
 											    NULL AS CREDIT,
-											    SUM(A.ST_TOTAL) AS FINAL
+											    A.ST_TOTAL AS FINAL
 										   FROM VW_STOCK_TRANSFER A,
 											    CUSTOMER B
 										  WHERE A.CUSTOMER_ID = B.ID
@@ -393,7 +346,7 @@ BEGIN
 												   WHEN \'',P_COMPANY_ID,'\' <> "" THEN A.COMPANY_FROM_ID = \'',P_COMPANY_ID,'\'
 												   ELSE TRUE
 											    END
-									   GROUP BY B.CUSTOMER_ID, A.ST_ENTRY_DATE, A.ST_ID,A.ID
+									   
 											  
 									     UNION ALL
 										
@@ -403,9 +356,9 @@ BEGIN
 											    A.PAYPAL_TRANSACTION_ID, 
 											    ''P'' AS FLAG,
 											    ''Payment Sent'' AS FORM,
-											    SUM(A.AMOUNT) AS DEBIT,
+											    A.AMOUNT AS DEBIT,
 											    NULL AS CREDIT,
-											    SUM(A.AMOUNT) AS FINAL
+											    A.AMOUNT AS FINAL
 										   FROM PAYMENT_SENT A,
 										  	    CUSTOMER B
 										  WHERE A.CUSTOMER_ID = B.ID
@@ -425,7 +378,7 @@ BEGIN
 												   WHEN \'',P_COMPANY_ID,'\' <> "" THEN A.COMPANY_ID = \'',P_COMPANY_ID,'\'
 												   ELSE TRUE
 											    END
-									   GROUP BY B.CUSTOMER_ID, A.PS_ENTRY_DATE, A.PAYPAL_TRANSACTION_ID,A.iD
+									   
 											  
 									     UNION ALL 
 										
@@ -436,8 +389,8 @@ BEGIN
 											    ''R'' AS FLAG, 
 											    ''Partial Credit'' AS FORM,
 											    NULL AS DEBIT,
-											    SUM(A.TOTAL_AMOUNT) AS CREDIT,
-											    SUM(A.TOTAL_AMOUNT * -1) AS FINAL
+											    A.TOTAL_AMOUNT AS CREDIT,
+											    A.TOTAL_AMOUNT * -1 AS FINAL
 										   FROM PARTIAL_CREDIT A,
 											    CUSTOMER B
 										  WHERE A.CUSTOMER_ID = B.ID
@@ -457,7 +410,7 @@ BEGIN
 												   WHEN \'',P_COMPANY_ID,'\' <> "" THEN A.COMPANY_ID = \'',P_COMPANY_ID,'\'
 												   ELSE TRUE
 											    END
-									   GROUP BY B.CUSTOMER_ID, A.PC_ENTRY_DATE, A.PAYPAL_TRANSACTION_ID,A.ID
+									   
 											  
 									     UNION ALL 
 									   
@@ -468,8 +421,8 @@ BEGIN
 											    ''Q'' AS FLAG, 
 											    ''Sale Return'' AS FORM,
 											    NULL AS DEBIT,
-											    SUM(A.SR_TOTAL) AS CREDIT,
-											    SUM(A.SR_TOTAL * -1) AS FINAL
+											    A.SR_TOTAL AS CREDIT,
+											    A.SR_TOTAL * -1 AS FINAL
 										   FROM SALE_RETURN A,
 											    CUSTOMER B
 										  WHERE A.CUSTOMER_ID = B.ID
@@ -489,7 +442,7 @@ BEGIN
 												   WHEN \'',P_COMPANY_ID,'\' <> "" THEN A.COMPANY_ID = \'',P_COMPANY_ID,'\'
 												   ELSE TRUE
 											    END
-									   GROUP BY B.CUSTOMER_ID, A.SR_ENTRY_DATE, A.PAYPAL_TRANSACTION_ID,A.ID
+									   
 											  
 									     UNION ALL
 
@@ -500,8 +453,8 @@ BEGIN
 											    ''M'' AS FLAG, 
 											    ''Receive Money'' AS FORM,
 											    NULL AS DEBIT,
-											    SUM(A.AMOUNT) AS CREDIT,
-											    SUM(A.AMOUNT * -1) AS FINAL
+											    A.AMOUNT AS CREDIT,
+											    A.AMOUNT * -1 AS FINAL
 										   FROM RECEIVE_MONEY A,
 											    CUSTOMER B
 										  WHERE A.CUSTOMER_ID = B.ID
@@ -521,7 +474,7 @@ BEGIN
 												   WHEN \'',P_COMPANY_ID,'\' <> "" THEN A.COMPANY_ID = \'',P_COMPANY_ID,'\'
 												   ELSE TRUE
 											    END
-									   GROUP BY B.CUSTOMER_ID, A.RM_ENTRY_DATE, A.PAYPAL_TRANSACTION_ID,A.ID
+									   
 											  
 									     UNION ALL
 
@@ -531,9 +484,9 @@ BEGIN
 											    A.PAYPAL_TRANSACTION_ID, 
 											    ''X'' AS FLAG, 
 											    ''Replacement Issue'' AS FORM,
-											    SUM(A.BALANCE) AS DEBIT,
+											    A.BALANCE AS DEBIT,
 											    NULL AS CREDIT,
-											    SUM(A.BALANCE) AS FINAL
+											    A.BALANCE AS FINAL
 										   FROM REPLACEMENT A,
 											    CUSTOMER B
 										  WHERE A.BALANCE >= 0
@@ -554,7 +507,7 @@ BEGIN
 												   WHEN \'',P_COMPANY_ID,'\' <> "" THEN A.COMPANY_ID = \'',P_COMPANY_ID,'\'
 												   ELSE TRUE
 											    END
-									   GROUP BY B.CUSTOMER_ID, A.REP_ENTRY_DATE, A.PAYPAL_TRANSACTION_ID,A.ID
+									   
 											  
 									     UNION ALL
 
@@ -565,8 +518,8 @@ BEGIN
 											    ''Y'' AS FLAG,
 											    ''Replacement Return'' AS FORM,
 											    NULL AS DEBIT,
-											    SUM(A.BALANCE * -1) AS CREDIT,
-											    SUM(A.BALANCE) AS FINAL
+											    A.BALANCE * -1 AS CREDIT,
+											    A.BALANCE AS FINAL
 										   FROM REPLACEMENT A,
 											    CUSTOMER B
 										  WHERE A.BALANCE < 0
@@ -587,7 +540,7 @@ BEGIN
 												   WHEN \'',P_COMPANY_ID,'\' <> "" THEN A.COMPANY_ID = \'',P_COMPANY_ID,'\'
 												   ELSE TRUE
 											    END
-									   GROUP BY B.CUSTOMER_ID, A.REP_ENTRY_DATE, A.PAYPAL_TRANSACTION_ID,A.ID
+									   
                                        
                                          UNION ALL
 
@@ -598,16 +551,16 @@ BEGIN
 											    ''T'' AS FLAG,
 											    ''Customer Receipt'' AS FORM,
                                                 CASE 
-													WHEN A.REMAINING_AMOUNT < 0 THEN SUM(ABS(A.REMAINING_AMOUNT)) 
+													WHEN A.REMAINING_AMOUNT < 0 THEN ABS(A.REMAINING_AMOUNT) 
 													ELSE NULL 
                                                 END AS DEBIT,
 												CASE 
-													WHEN A.REMAINING_AMOUNT > 0 THEN SUM(ABS(A.REMAINING_AMOUNT)) 
+													WHEN A.REMAINING_AMOUNT > 0 THEN ABS(A.REMAINING_AMOUNT) 
 													ELSE NULL  
                                                 END AS CREDIT,
 												CASE 
-													WHEN A.REMAINING_AMOUNT > 0 THEN SUM(ABS(A.REMAINING_AMOUNT) * -1) 
-													ELSE SUM(ABS(A.REMAINING_AMOUNT))
+													WHEN A.REMAINING_AMOUNT > 0 THEN ABS(A.REMAINING_AMOUNT) * -1 
+													ELSE ABS(A.REMAINING_AMOUNT)
                                                 END AS FINAL
 										   FROM RECEIPTS A,
 											    CUSTOMER B
@@ -629,7 +582,6 @@ BEGIN
 												   WHEN \'',P_COMPANY_ID,'\' <> "" THEN A.COMPANY_ID = \'',P_COMPANY_ID,'\'
 												   ELSE TRUE
 											    END
-									   GROUP BY B.CUSTOMER_ID, A.R_ENTRY_DATE, A.REC_REFERENCE,A.ID
 									   ) C
 							     WHERE CASE
 										  WHEN \'',P_FORM_TYPE,'\' <> "-1" THEN FLAG IN (',P_FORM_TYPE,')
